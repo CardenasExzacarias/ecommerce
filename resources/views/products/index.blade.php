710bf7808a072f717581516754b370230a928e1a
Kevin-Yahir-MN <kevinyahirmn@outlook.com>
2025-01-09 19:48:52 -0800

    Productos
    Re-edición de la tabla

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -17,16 +17,17 @@
     </div>
     <div class="p-6 bg-white rounded-lg shadow-md">
         <h2 class="mb-4 text-xl font-bold">Lista de productos</h2>
-        <div class="flex justify-between items-center mb-4">
+        <div class="flex items-center mb-4">
             <input
                 type="text"
                 placeholder="Buscar producto..."
                 class="px-4 py-2 w-1/3 rounded-md border border-gray-300"
             />
-            <a href="{{ route('product.create') }}" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">
+            <a href="{{ route('product.create') }}" class="px-4 py-2 ml-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">
                 Crear Producto
             </a>
         </div>
+
 
         <table class="w-full border border-gray-300 border-collapse table-auto">
             <thead class="text-gray-700 bg-orange-100">
@@ -48,16 +49,16 @@
                         <td class="px-6 py-4 text-center border-b">
                             <div class="flex justify-center space-x-2">
                                 <a href="{{ route('product.show', $product->id) }}" class="text-green-500 hover:text-green-700">
-                                    @svg('eva-eye', 'w-10 h-10 inline-block')
+                                    @svg('eva-eye', 'w-8 h-8 inline-block')
                                 </a>
                                 <a href="{{ url('product/' . $product->id . '/edit') }}" class="text-blue-500 hover:text-blue-700">
-                                    @svg('eva-edit', 'w-10 h-10 inline-block')
+                                    @svg('eva-edit', 'w-8 h-8 inline-block')
                                 </a>
                                 <form action="{{ route('product.destroy', $product->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="text-red-500 hover:text-red-700">
-                                        @svg('eva-trash', 'w-10 h-10 inline-block')
+                                        @svg('eva-trash', 'w-8 h-8 inline-block')
                                     </button>
                                 </form>
                             </div>
