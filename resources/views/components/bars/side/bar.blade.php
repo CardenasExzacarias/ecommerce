909ab91bea0c7b54d482b84211349351e24b91d8
Kevin-Yahir-MN <kevinyahirmn@outlook.com>
2025-01-23 18:57:37 -0800

    Esqueleto bar
    Se agrego el cuerpo de la sidebar en el componente bar

--- resources/views/components/bars/side/bar.blade.php
+++ resources/views/components/bars/side/bar.blade.php
@@ -1,3 +1,7 @@
-<ul>
-    <li>Sidebar</li>
-</ul>
+<aside
+  :class="sidebarToggle ? 'translate-x-0' : '-translate-x-full'"
+  class="absolute left-0 top-0 z-9999 flex h-screen flex-col overflow-y-hidden bg-black border-r border-strokedark duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0"
+  @click.outside="sidebarToggle = false"
+>
+  <!-- Barra lateral vacía con borde -->
+</aside>
